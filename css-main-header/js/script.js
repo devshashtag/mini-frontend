fc9f69a05af463b4096bfa2b0a89ba0c8b42b42d
devs.hashtag <devs.hashtag@gmail.com>
2022-10-14 10:43:57 +0330

    css-main-header project added

---
+++ css-main-header/js/script.js
@@ -0,0 +1,18 @@
+const links = document.querySelector('.links');
+const lastActive = links.querySelector('.active');
+
+links.addEventListener('click', (el) => {
+  // select li from a
+  const elm = el.target.parentNode;
+
+  if (elm.tagName.toLowerCase() === 'li') {
+    // active new btn
+    elm.classList.add('active');
+
+    // remove last btn
+    if (lastActive) lastActive.classList.remove('active');
+
+    // save active btn
+    lastActive = elm;
+  }
+});
